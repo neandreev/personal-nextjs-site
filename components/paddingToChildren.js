ffc9c55be2bed46b165ba19795c54dc7dfca9d2d
Andreev Danila <mail@neandreev.ru>
2020-10-22 09:46:13 +0500

    Change padding on small screens

--- components/paddingToChildren.js
+++ components/paddingToChildren.js
@@ -2,7 +2,7 @@ export default function PaddingToChildren({ children }) {
   return (
     <>
       {children.map((child, index) => (
-        <div key={index} className="p-8">
+        <div key={index} className="p-4 md:p-8">
           {child}
         </div>
       ))}
