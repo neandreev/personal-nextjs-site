ddf5dda6e045dc86bbe4588dc65c18daba0ac1a6
Andreev Danila <mail@neandreev.ru>
2020-10-22 09:19:11 +0500

    Projects descriptions correction

--- components/projects.js
+++ components/projects.js
@@ -2,8 +2,17 @@ import List from "../components/list"
 import Paragraph from "../components/paragraph"
 
 const projects = [
-  { name: 'Brain-games', description: 'Набор из пяти CLI завязанных на математике игр', properties: ['Vanilla JS'] },
-  { name: 'Gendiff', description: 'Some description here', properties: ['first', 'second'] },
+  {
+    name: "Brain-games",
+    description: "Набор из пяти CLI завязанных на математике игр",
+    properties: ["Vanilla JS"],
+  },
+  {
+    name: "Gendiff",
+    description:
+      "CLI-Утилита, позволяющая находить и выводить на экран различия между двумя конфигурационными файлами формата json, yml или ini",
+    properties: ["Vanilla JS", "Commander", "Parsers: ini, js-yaml", "Testing: jest"],
+  },
 ]
 
 export default function Projects() {
@@ -11,21 +20,29 @@ export default function Projects() {
     <>
       <span>Проекты:</span>
       <div className="flex flex-wrap md:flex-no-wrap justify-between">
-      {projects.map((project, index) => {
-        const { name, description, properties } = project;
-        const imageUrl = `/images/${name}.png`
+        {projects.map((project, index) => {
+          const { name, description, properties } = project
+          const imageUrl = `/images/${name}.png`
+          const imageAlt = `${name} project representation`
 
-        return ( 
-          <div key={index} className="mt-5 mx-1 p-2 w-full md:w-10/12 border rounded shadow-sm">
-            <span className="font-bold">{name}</span>
-            <img className="rounded my-4" src={imageUrl} alt="Brain-games project representation" />
-            <span>О чём:</span>
-            <Paragraph>{description}</Paragraph>
-            <span>При помощи чего:</span>
-            <List points={properties} />
-          </div>
-        )})
-      }
+          return (
+            <div
+              key={index}
+              className="mt-5 mx-1 p-2 w-full md:w-10/12 border rounded shadow-sm"
+            >
+              <span className="font-bold">{name}</span>
+              <img
+                className="rounded my-4"
+                src={imageUrl}
+                alt={imageAlt}
+              />
+              <span>О чём:</span>
+              <Paragraph>{description}</Paragraph>
+              <span>При помощи чего:</span>
+              <List points={properties} />
+            </div>
+          )
+        })}
       </div>
     </>
   )
