cd878bb645f6baa0f99ad9207d06a55e753f43cf
Andreev Danila <mail@neandreev.ru>
2020-10-25 13:05:17 +0500

    Change hardskills structure

--- components/skills.js
+++ components/skills.js
@@ -1,17 +1,27 @@
 import List from "../components/list";
 
-const hardskills = ["HTML, CSS", "JavaScript", "React, Redux", "Jest", "Декларативный код", "Linux power user", ];
+const presenthardskills = ["HTML, CSS", "JavaScript", "React, Redux", "Jest", "Декларативный код", "Linux power user", ];
+const futurehardskills = ["SASS", "TypeScript"]
 const softskills = ["Critical thinking", "Lifelong learner", "Self-Reflection", "Nonviolent Communication", "Emotional intelligence"];
 
 export default function Skills() {
   return (
     <div className="flex flex-wrap md:flex-no-wrap justify-between">
       <div className="mx-1 p-2 w-full md:w-10/12 border rounded shadow-sm">
-        <span>Hardskills:</span>
-        <List points={hardskills} />
+        <span className="pl-5">Hardskills:</span>
+        <div className="flex justify-between">
+          <div className="w-10/12">
+            <span className="pl-5">In use:</span>
+            <List points={presenthardskills} />
+          </div>
+          <div className="w-10/12">
+            <span className="pl-5">In progress:</span>
+            <List points={futurehardskills} />
+          </div>
+        </div>
       </div>
       <div className="mt-5 md:mt-0 mx-1 p-2 w-full md:w-10/12 border rounded shadow-sm">
-        <span>Softskills:</span>
+        <span className="pl-5">Softskills:</span>
         <List points={softskills} />
       </div>
     </div>
