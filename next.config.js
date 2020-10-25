8c46469f1d24a06abaca0ee76a19ed99320ca29c
Andreev Danila <mail@neandreev.ru>
2020-10-25 11:23:32 +0500

    Fix github redirect

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/github',
-        destination: 'https://github.com/neandreev/personal-nextjs-site',
+        destination: 'https://github.com/neandreev/',
         permanent: true,
       },
     ]
