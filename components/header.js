5347a479cddabe391a7851670f26db9bc8991ade
Andreev Danila <mail@neandreev.ru>
2020-10-28 18:02:32 +0500

    Add desktop-only working profile photo

--- components/header.js
+++ components/header.js
@@ -1,12 +1,17 @@
 export default function Header() {
   return (
-    <>
-      <h1 className="text-5xl text-center text-accent-1">
-        Андреев Данила
-      </h1>
-      <h2 className="text-3xl text-center">
-        Фронтенд-разработчик
-      </h2>
-    </>
+    <div className="flex items-center justify-center">
+      <div className="mx-4">
+        <img className="rounded-full" width="128px" src='/images/Profile-photo-gray.png' alt='My photo' />
+      </div>
+      <div className="text-left mx-4">
+        <h1 className="text-5xl text-accent-1">
+          Андреев Данила
+        </h1>
+        <h2 className="text-3xl">
+          Фронтенд-разработчик
+        </h2>
+      </div>
+    </div>
   )
 }
