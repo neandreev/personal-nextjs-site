ecaac76cc05d6354b7c26d2db3db022bace4d959
Andreev Danila <mail@neandreev.ru>
2021-08-17 16:32:48 +0500

    update redirects

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ module.exports = {
         destination: 'https://github.com/neandreev/',
         permanent: true,
       },
+      {
+        source: '/telegram',
+        destination: 'https://t.me/neandreev',
+        permanent: true,
+      },
+      {
+        source: '/mail',
+        destination: 'mailto:mail@neandreev.ru',
+        permanent: true,
+      }
     ];
   },
 };
