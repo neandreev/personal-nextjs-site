6260b6ac7ab27448abf8e1a7c6b1a97286f2fd4a
Andreev Danila <mail@neandreev.ru>
2021-08-19 00:27:14 +0500

    update 'footer' section

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,10 @@
+import Link from './link';
+
 const Footer = () => (
   <div className="bg-white border border-b-0 flex-shrink-0 mt-5 py-2 text-center shadow-2xl rounded-t">
     Сделано на Next.js + Tailwind CSS (
-    <a
-      className="underline"
-      target="_blank"
-      rel="noreferrer"
-      href="https://github.com/neandreev/personal-nextjs-site"
-    >
-      GitHub
-    </a>
-    )
+    <Link href="https://github.com/neandreev/personal-nextjs-site" text="GitHub" />
+    ); Развёрнуто на <Link text="Vercel" href="https://vercel.com" />
   </div>
 );
 
