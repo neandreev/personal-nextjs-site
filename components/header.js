c987423c5b6523f387e202c05a561f4387895fcb
Andreev Danila <mail@neandreev.ru>
2021-09-02 18:57:37 +0500

    change profile image border

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ const Header = () => (
   <div className="flex flex-col items-center md:items-start justify-between leading-tight">
     <div className="flex justify-center">
       <div className="hidden relative md:bg-gray-100 rounded-full border border-r-0 rounded-r-none h-full md:block w-32">
-        <img className="rounded-full border-4" src="/images/photo.png" alt="photo"/>
+        <img className="rounded-full border-2" src="/images/photo.png" alt="photo"/>
       </div>
       <div className="md:border md:border-l-0 pr-6 p-3 pt-1 md:rounded-full md:bg-gray-100 md:rounded-full md:rounded-l-none flex flex-col justify-center">
         <h2 className="text-5xl text-center md:text-left">
