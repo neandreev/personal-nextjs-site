e954ddaa84dacb23ed02fe694c9324ca03412ffc
Andreev Danila <mail@neandreev.ru>
2021-09-02 18:51:25 +0500

    update profile image

--- components/header.js
+++ components/header.js
@@ -3,10 +3,10 @@ import Link from './link';
 const Header = () => (
   <div className="flex flex-col items-center md:items-start justify-between leading-tight">
     <div className="flex justify-center">
-      <div className="hidden relative h-full md:block w-32">
-        <img className="rounded-3xl rounded-r-none" src="/images/photo.jpg" alt="photo"/>
+      <div className="hidden relative md:bg-gray-100 rounded-full border border-r-0 rounded-r-none h-full md:block w-32">
+        <img className="rounded-full border-4" src="/images/photo.png" alt="photo"/>
       </div>
-      <div className="md:border pr-6 p-3 pt-1 md:rounded-full md:bg-gray-100 md:rounded-full md:rounded-l-none flex flex-col justify-center">
+      <div className="md:border md:border-l-0 pr-6 p-3 pt-1 md:rounded-full md:bg-gray-100 md:rounded-full md:rounded-l-none flex flex-col justify-center">
         <h2 className="text-5xl text-center md:text-left">
           Андреев Данила
         </h2>
