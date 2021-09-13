30e43d211c61ddac9560686eff5366caf5a90a19
Andreev Danila <mail@neandreev.ru>
2021-09-13 11:44:44 +0500

    update roundness of elements

--- components/container.js
+++ components/container.js
@@ -1,9 +1,9 @@
 import Footer from './footer';
 
 const Container = ({ children }) => (
-  <div className="flex text-accent-1 justify-center pt-0 md:pt-8 mx-4 min-h-screen">
+  <div className="flex text-accent-1 justify-center pt-8 mx-4 min-h-screen">
     <div className="flex flex-col justify-between max-w-screen-lg">
-      <div className="flex flex-col border border-t-0 md:border rounded-b shadow-2xl w-full md:rounded-t divide-y">
+      <div className="flex flex-col border rounded-lg shadow-2xl w-full divide-y">
         {children.map((child, index) => (
           <div id={child.props.id} key={index} className="p-4 md:p-8">
             {child}
--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 import Link from './link';
 
 const Footer = () => (
-  <div className="bg-white border border-b-0 flex-shrink-0 mt-5 py-2 text-center shadow-2xl rounded-t">
+  <div className="bg-white border border-b-0 flex-shrink-0 mt-5 py-2 text-center shadow-2xl rounded-t-lg">
     Сделано на Next.js + Tailwind CSS (
     <Link href="https://github.com/neandreev/personal-nextjs-site" text="GitHub" />
     ); Развёрнуто на <Link text="Vercel" href="https://vercel.com" />
