4f73639161b573ec34944df71e4b410ab3388ec7
Andreev Danila <mail@neandreev.ru>
2021-09-29 13:48:09 +0500

    add the site itself into "projects" section

--- components/projects.js
+++ components/projects.js
@@ -1,12 +1,14 @@
 import List from './list';
 import Link from './link';
 import Paragraph from './paragraph';
+import React from 'react';
 
 const projects = [
   {
     name: 'Brain-games',
     description: 'Набор из пяти command-line игр, завязанных на математике',
     properties: ['Vanilla JS'],
+    gifUrl: '/images/Brain-games.gif',
     githubUrl: 'https://github.com/neandreev/Brain-games',
   },
   {
@@ -14,6 +16,7 @@ const projects = [
     description:
       'CLI-Утилита, позволяющая находить и выводить на экран различия между двумя конфигурационными файлами формата json, yml или ini',
     properties: ['Vanilla JS', 'Commander', 'Parsers: ini, js-yaml', 'Testing: jest'],
+    gifUrl: '/images/Gendiff.gif',
     githubUrl: 'https://github.com/neandreev/Gendiff',
   },
   {
@@ -22,23 +25,40 @@ const projects = [
       'Агрегатор RSS фидов, производящий парсинг RSS по приведённым вами ссылкам и выводящий в простой форме посты',
     properties: ['Vanilla JS', 'Webpack', 'Validation: yup', 'Internationalization: i18next'],
     liveDemonstrationUrl: 'https://rss.neandreev.ru',
+    gifUrl: '/images/RSS-Agregator.gif',
     githubUrl: 'https://github.com/neandreev/RSS-Agregator',
   },
+  {
+    name: 'Personal-site',
+    description:
+      'Личный сайт-визитка, содержащий в себе мои контакты, немного информации обо мне и выполненные мною проекты',
+    properties: ['NextJS', 'React', 'Styling: Tailwind CSS'],
+    liveDemonstrationUrl: 'https://neandreev.ru',
+    githubUrl: 'https://github.com/neandreev/personal-nextjs-site',
+  }
 ];
 
 const renderLinks = (project) => {
-  const { name, liveDemonstrationUrl, githubUrl } = project;
-  const gifUrl = `/images/${name}.gif`;
+  const { gifUrl, liveDemonstrationUrl, githubUrl } = project;
+  const links = [];
 
   const githubLink = <Link href={githubUrl} text='GitHub' />;
-  const gifLink = <Link href={gifUrl} text="GIF Demonstration" />;
+  console.log(githubLink);
+  links.push(githubLink);
 
-  if (!liveDemonstrationUrl) {
-    return <p>{githubLink}, {gifLink}</p>;
+  if (gifUrl) {
+    const gifLink = <Link href={gifUrl} text="GIF Demonstration" />;
+    links.push(gifLink);
   }
 
-  const demonstrationLink = <Link href={liveDemonstrationUrl} text="Live Demonstration" />;
-  return <p>{githubLink}, {gifLink}, {demonstrationLink}</p>;
+  if (liveDemonstrationUrl) {
+    const liveDemonstrationLink = <Link href={liveDemonstrationUrl} text="Live demonstration" />;
+    links.push(liveDemonstrationLink);
+  }
+
+  return <ul>
+    {links.map(link => <li className="inline pr-2">{link}</li>)}
+  </ul>
 };
 
 const renderProject = (project, index) => {
